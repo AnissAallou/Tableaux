c5f753c7d53e189b67c6cdf7921537605921d8c3
anisskiller <anissaallou36@gmail.com>
2018-11-16 10:04:38 +0100

    ptdr

--- src/tableau1/Exo10.java
+++ src/tableau1/Exo10.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class Exo10 {
 	public static void main(String [] args) 
 	{ 
+		//Toujours et encore sur le même principe, écrivez un algorithme permettant, 
+		// à l’utilisateur de saisir les notes d'une classe.
+		// Le programme, une fois la saisie terminée, renvoie le nombre des notes supérieures 
+		// à la moyenne de la classe.
 		
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
@@ -27,17 +31,19 @@ public class Exo10 {
 					tabble[i] = Integer.parseInt(sc.nextLine());
 					som = tabble[i];
 					while(tabble[i] > 20|| tabble[i] < 0) { 
-						System.out.println("Un note c'est entre 0 et 20 tocard :p");
+						System.out.println("Un note c'est entre 0 et 20");
 						tabble[i] = Integer.parseInt(sc.nextLine());
+						// On vérifie que l'utilisateur renseigne une note entre 0 et 20
 					}
 					
 			}
 
-			moy = som/n;
+			moy = som/n; // on calcule la moyenne
 			
 			for(int i=0; i < n; i++) {
 					if(moy < tabble[i]) {
-						moysup++;
+						moysup++; 
+						// on détermine le nombre de moyennes supérieures à celle de la classe
 					}
 			}
 
--- src/tableau1/Exo9.java
+++ src/tableau1/Exo9.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class Exo9 {
 	public static void main(String [] args) 
 	{ 
+		// Ecrivez un algorithme permettant, toujours sur le même principe, 
+		// à l’utilisateur de saisir un nombre déterminé de valeurs.
+		// Le programme, une fois la saisie terminée, renvoie la plus grande valeur 
+		// en précisant quelle position elle occupe dans le tableau.
+		// On prendra soin d’effectuer la saisie dans un premier temps, 
+		// et la recherche de la plus grande valeur du tableau dans un second temps.
 		
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
@@ -23,6 +29,7 @@ public class Exo9 {
 					System.out.println("Entrez le nombre numéro : " + (i+1));
 					tbl[i] = Integer.parseInt(sc.nextLine());
 					if(tbl[i] > plusgrand) {
+						// on effectue le test pour déterminer la valeur la plus grande du tableau 
 						plusgrand = tbl[i];
 						position = i + 1;
 					}
