b26ce631b55cd1fa2b1f80b826fda00caf270c62
anisskiller <anissaallou36@gmail.com>
2018-11-16 11:39:52 +0100

    Update Exo1.java

--- src/tableau2/Exo1.java
+++ src/tableau2/Exo1.java
@@ -1,6 +1,8 @@
 package tableau2;
 
 public class Exo1 {
+	
+	// Écrivez un algorithme remplissant un tableau de 6 sur 13, avec des zéros.
 	 
 	public static void main(String[] args) {
 	
@@ -9,7 +11,8 @@ public class Exo1 {
 		for(int i=0; i<6; i++) {
 		  for(int j=0; j<13; j++) {
 		    tab[i][j] = 0;
-		    System.out.print(tab[i][j] + " ");
+		    System.out.print(tab[i][j] + " "); // On affecte la valeur zéro à toutes les cases du tableau
+		    // Aussi bien en ligne qu'en colonne
 		}
 		  
 		  System.out.println(" ");
