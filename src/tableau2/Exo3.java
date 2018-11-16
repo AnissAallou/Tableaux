910faec4ab394b269c9042c95c61031cdd3bbdc6
anisskiller <anissaallou36@gmail.com>
2018-11-16 12:13:42 +0100

    Update Exo3.java

--- src/tableau2/Exo3.java
+++ src/tableau2/Exo3.java
@@ -1,13 +1,16 @@
 package tableau2;
 
 public class Exo3 {
+
+	// Cet algorithme remplit un tableau avec huit valeurs : 0, 1, 0, 1, 0, 1, 0, 1.
+	// Cela ressemble à du binaire :p
 	
 	public static void main(String[] args) {
 			
 		 
 		System.out.println("Exercice 3 Tableaux multidimensionnels");
 		
-		int[][] tableaux = new int[4][2]; 
+		int[][] tableaux = new int[4][2]; // On déclare un tableau de dimensions 4*2
 		
 		for(int i=3; i>=0; i--) {
 			for(int j=1; j>=0; j--) {
@@ -15,7 +18,7 @@ public class Exo3 {
 			}
 		}
 		
-		for(int i=0; i<=3; i++) {
+		for(int i=0; i<=3; i++) { // On décrémente
 			for(int j=0; j<=1; j++) {
 				System.out.print(tableaux[i][j] + " ");
 			}
