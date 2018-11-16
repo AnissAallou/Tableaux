ac03bb68789b54a1817528199ad99011d7755150
anisskiller <anissaallou36@gmail.com>
2018-11-16 12:02:50 +0100

    Update Exo2.java

--- src/tableau2/Exo2.java
+++ src/tableau2/Exo2.java
@@ -1,6 +1,8 @@
 package tableau2;
 
 public class Exo2 { 
+	// Cet algorithme remplit un tableau avec les six valeurs : 0, 1, 2, 3, 4, 5. 
+	// Il les écrit ensuite à l’écran. 
 	
 	public static void main(String[] args) {
 	
@@ -22,6 +24,18 @@ public class Exo2 {
 			}
 		}
 		
+		
+		System.out.println("\n" + "\n" + "Méthode simplifiée : ");
+		// On fait tout en une boucle 
+		for(int i=0; i<=1; i++) {
+			for(int j=0; j<=2; j++) {		
+				System.out.print(tableau[i][j] + " ");
+				tableau[i][j] = valeur;
+				valeur++;
+				
+				
+			}
+		}
 		
 		
 	}
