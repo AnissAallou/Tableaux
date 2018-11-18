38b4f1d13ba9dc955c9532be1ded30a87fa371a0
anisskiller <anissaallou36@gmail.com>
2018-11-18 22:46:20 +0100

    Update TestArrayTri.java

--- src/tp03/TestArrayTri.java
+++ src/tp03/TestArrayTri.java
@@ -1,16 +1,20 @@
 package tp03;
 
 import java.util.Arrays;
+// import de la classe Arrays
 
- 
+  
 public class TestArrayTri {
 
 		public static void main(String[] args) {
 			
 			int array[] = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
 			
-			Arrays.sort(array);
 			
+			// Tri du tableau 
+			
+			// La méthode to String on permet de renvoyer une chaîne de caractères 
+			// servant à décrire l'objet concerné
 			System.out.printf(Arrays.toString(array));
 			 
 		}
