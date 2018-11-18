abc9ded13feec086dfcb8fa4076867dc06075b49
anisskiller <anissaallou36@gmail.com>
2018-11-18 22:41:42 +0100

    Update TestArray2.java

--- src/tp03/TestArray2.java
+++ src/tp03/TestArray2.java
@@ -6,49 +6,25 @@ public class TestArray2 {
 			
 			int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
 			int index = 0;
-		
-			
+			int repeat =0;
+				
 		    float somme = 0;
 			for (int i=0; i<array.length;i++) {
 		         System.out.print(array[i]+" ");
-		         somme += array[i];
-		         if(array[i] == 15) {
-		        	 index = i;
-		         }
-		         
-		         
-		      }
-		      float moyenne = somme / array.length;
-		 
-		      System.out.print("\nMoyenne = "+ moyenne);
-		      
-		      
-		      System.out.println("\n");; 
-		      
-		      System.out.println("L'index de l'entier 15 : " + index);
-		   // l'index de l'entier 15 est 1 
-		    
-		      
-		      System.out.println("\n");; 
-		      
-		      System.out.println("Les doublons dans le tableau : ");
-		      
-		      int temp;
-		      // boucle
-		      
-		      for(int i = 0; i < array.length; i++) {
-		    	
-		    	  if(array[i] > array[i+1]) { 
-		    			  temp = array [i];
-		    			  array[i] = array[i+1];
-		    			  array[i+1] = temp;
-		    	  }
-		    	  System.out.print(array[i]);
-		      }
-		      
-		      
-		      
-		      
+		         somme += array[i]; // on additionne les éléments du tableau
+		         if(array[i]==15) {
+				index = i;
+				} for(int j = i+1; j < array.length; j++) {
+					if(array[i] == array[j]) { // Déterminer le nombre de doublons
+						repeat++; // On compte le nombre de doublons à chaque tour de boucle
+					}
+				}
+		     }
+		     
+		    float moyenne = somme / array.length;
+		    System.out.println(" ");
+		    System.out.print("\nMoyenne = "+ moyenne + "\n\nL'index de l'entier 15 : " + index + "\n\n" + repeat + " doublons dans le tableau");
+		     		      		      		      		      
 		      
 		}
 }
