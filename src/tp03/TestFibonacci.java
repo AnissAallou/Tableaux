214b1cda7a1e29eafb372a487d7cc8e3837dbe4a
anisskiller <anissaallou36@gmail.com>
2018-11-18 22:47:09 +0100

    Update TestFibonacci.java

--- src/tp03/TestFibonacci.java
+++ src/tp03/TestFibonacci.java
@@ -9,26 +9,26 @@ public class TestFibonacci {
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		
-		// Demander à l’utilisateur de choisir un rang N
+		// Demander Ã  lâ€™utilisateur de choisir un rang N
 		System.out.println("Saisir un rang N : ");
 		int rangN = sc.nextInt();
 		
-		// On implémente la méthode écrite en bas en utilisant rangN en tant qu'argument
+		// On implÃ©mente la mÃ©thode Ã©crite en bas en utilisant rangN en tant qu'argument
 		System.out.println("le nombre est de "+ calculeAfficheRang(rangN));
 		
 	}
 	
 	
-	// Méthode qui calcule et affiche le nombre de rang N  
+	// MÃ©thode qui calcule et affiche le nombre de rang N  
 	public static int calculeAfficheRang(int rangN) {
 
 		int tableau [] = new int [rangN + 2];
 		tableau[0]=0;
 		tableau[1]=1;
 	
-			for(int i=2; i<=rangN; i++) {
-				tableau[i]=tableau[i-2]+tableau[i-1];
-			}
+		for(int i=2; i<=rangN; i++) {
+			tableau[i]=tableau[i-2]+tableau[i-1];
+		}
 		return tableau[rangN-1];
 	}
 
